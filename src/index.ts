39ff51a8d189fd4630b47c7f6a4bea87d8e65b25
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2023-05-18 16:21:11 +0900

    Initialize Project (#1)
    * feat: eslint, prettier
    
    * set dependencies
    
    * Initialize express app
    
    * initialize docker

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import configVar from './util/configVariable';
+
+const app = express();
+const port = configVar.server.port;
+
+app.listen(port, () => {
+console.log(`connect server at port number ${port}...`);
+});
