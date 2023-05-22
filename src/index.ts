3e6f2e28aa0e3b7a2aa8c54def0254a3c7b2e843
chipmunk-dev <devquokkajeong@gmail.com>
2023-05-22 21:18:28 +0900

    #10 - app에서 데이터 베이스 연결

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import configVar from './util/configVariable';
+import { AppDataSource } from './db/db';
 
 const app = express();
 const port = configVar.server.port;
 
-app.listen(port, () => {
-  console.log(`connect server at port number ${port}...`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log('connect Database...');
+
+    app.listen(port, () => {
+      console.log(`connect server at port number ${port}...`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error: Database Initialize -> ', err);
+  });
