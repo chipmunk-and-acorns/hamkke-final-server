678b880a91d2c540ffe75893ad64ed7c6784bfb6
chipmunk-dev <devquokkajeong@gmail.com>
2023-05-26 17:52:38 +0900

    #19 - bcrypt와 jwt 적용

--- src/controller/member.ts
+++ src/controller/member.ts
@@ -1,41 +1,62 @@
 import { Request, Response } from 'express';
 import { isEmpty } from 'lodash';
-import * as MemberRepository from '../repository/member';
 import { MemberPostDto } from '../types/member';
+import { generateToken, hashPassword, matchPassword } from '../util/auth';
+import * as MemberRepository from '../repository/member';
 import Member from '../entity/member';
+import config from '../config/configVariable';
+import redisClient from '../db/redis';
+
+const generateAccessRefreshToken = (payload: { memberId: number }) => {
+  const { secretKey, accessExpiresIn, refreshKey, refreshExpiresIn } =
+    config.auth.jwt;
+  const accessToken = generateToken(payload, secretKey, accessExpiresIn);
+  const refreshToken = generateToken(payload, refreshKey, refreshExpiresIn);
+  return { accessToken, refreshToken };
+};
 
 /**
  * 회원가입
  */
 export const registerAccount = async (request: Request, response: Response) => {
   const { username, password, nickname, birth } = request.body;
-
   const findMember = MemberRepository.findMemberByUsername(username);
+
   if (!isEmpty(findMember)) {
     return response.status(409).json({ message: '이미 존하는 이메일입니다.' });
   }
 
   const [year, month, day] = birth;
   const birthDate = new Date(year, month, day);
-
-  // TODO: bcrypt로 비밀번호 암호화 하기
+  const hashingPassword = await hashPassword(
+    config.auth.bcrypt.saltRounds,
+    password,
+  );
 
   const postMemberData: MemberPostDto = {
     username,
-    password,
+    password: hashingPassword,
     nickname,
     birth: birthDate,
   };
+
   const createMember: Member = await MemberRepository.saveMember(
     postMemberData,
   );
+  const payload = {
+    memberId: createMember.memberId,
+  };
 
-  // TODO: jwt를 이용하여 Token을 발급
-  // TODO: refresh Token을 redis에 저장
+  const { accessToken, refreshToken } = generateAccessRefreshToken(payload);
 
-  return response
-    .status(201)
-    .json({ memberId: createMember.memberId, nickname: createMember.nickname });
+  await redisClient.set(createMember.memberId.toString(), refreshToken);
+
+  return response.status(201).json({
+    memberId: createMember.memberId,
+    nickname: createMember.nickname,
+    accessToken,
+    refreshToken,
+  });
 };
 
 /**
@@ -49,26 +70,34 @@ export const login = async (request: Request, response: Response) => {
       .status(401)
       .json({ message: '이메일 또는 암호가 일치하지 않습니다.' });
   };
+
   if (isEmpty(findMember)) {
     return returnUnMatch();
   }
 
-  // TODO: bcrypt 적용하였을때 수정할 곳
-  if (password !== findMember.password) {
+  if (!(await matchPassword(password, findMember.password))) {
     return returnUnMatch();
   }
 
-  // TODO: accessToken과 refreshToken을 생성
-  // TODO: redis에 refreshToken 저장하기
   const { memberId, nickname } = findMember;
-  return response.status(200).json({ memberId, nickname });
+  const payload = { memberId };
+  const { accessToken, refreshToken } = generateAccessRefreshToken(payload);
+
+  await redisClient.set(memberId.toString(), refreshToken);
+
+  return response
+    .status(200)
+    .json({ memberId, nickname, accessToken, refreshToken });
 };
 
 /**
  * 로그아웃
  */
 export const logout = async (_request: Request, response: Response) => {
-  // TODO: token 제거
+  const { memberId } = response.locals;
+
+  await redisClient.del(memberId.toString());
+
   return response.sendStatus(204);
 };
 
@@ -76,9 +105,9 @@ export const logout = async (_request: Request, response: Response) => {
  * 비밀번호 변경
  */
 export const updatePassword = async (request: Request, response: Response) => {
-  const { memberId } = request.params;
+  const { memberId } = response.locals;
   const { password, newPassword } = request.body;
-  const findMember = await MemberRepository.findMemberById(parseInt(memberId));
+  const findMember = await MemberRepository.findMemberById(memberId);
   if (isEmpty(findMember)) {
     return response
       .status(401)
@@ -86,11 +115,11 @@ export const updatePassword = async (request: Request, response: Response) => {
   }
 
   // TODO: bcrypt 적용후 코드 수정
-  if (password !== findMember.password) {
+  if (!(await matchPassword(password, findMember.password))) {
     return response.status(401).json({ message: '비밀번호가 불일치합니다.' });
   }
 
-  if (newPassword === findMember.password) {
+  if (await matchPassword(newPassword, findMember.password)) {
     return response
       .status(401)
       .json({ message: '변경하려는 비밀번호가 이전 비밀번호와 일치합니다.' });
@@ -106,9 +135,9 @@ export const updatePassword = async (request: Request, response: Response) => {
  * 닉네임 변경
  */
 export const updateNickname = async (request: Request, response: Response) => {
-  const { memberId } = request.params;
+  const { memberId } = response.locals;
   const { nickname } = request.body;
-  const findMember = await MemberRepository.findMemberById(parseInt(memberId));
+  const findMember = await MemberRepository.findMemberById(memberId);
   if (isEmpty(findMember)) {
     return response
       .status(401)
@@ -130,16 +159,17 @@ export const updateNickname = async (request: Request, response: Response) => {
 /**
  * 회원탈퇴
  */
-export const deleteAccount = async (request: Request, response: Response) => {
-  const { memberId } = request.params;
-  const findMember = await MemberRepository.findMemberById(parseInt(memberId));
+export const deleteAccount = async (_request: Request, response: Response) => {
+  const { memberId } = response.locals;
+  const findMember = await MemberRepository.findMemberById(memberId);
   if (isEmpty(findMember)) {
     return response
       .status(401)
       .json({ message: '존재하지 않은 사용자입니다.' });
   }
 
-  // TODO: redis에 해당 멤버의 refreshToken 삭제
-  await MemberRepository.deleteMemberById(parseInt(memberId));
+  await MemberRepository.deleteMemberById(memberId);
+  await redisClient.del(memberId.toString());
+
   return response.sendStatus(204);
 };
