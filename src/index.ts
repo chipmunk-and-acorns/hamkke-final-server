4d90bf6b72c442ba3ba403914cb27512bf4d94a4
chipmunk-dev <devquokkajeong@gmail.com>
2023-06-07 16:35:12 +0900

    fix: 코드 스타일 수정

--- src/index.ts
+++ src/index.ts
@@ -13,43 +13,58 @@ import SwaggerOptions from './config/swagger';
 import configVar from './config/configVariable';
 
 const app = express();
+const env = process.env.NODE_ENV;
 const port = configVar.server.port;
 const spec = swaggerJsdoc(SwaggerOptions);
-const env = process.env.NODE_ENV;
+const corsOption: cors.CorsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'authorization'],
+  preflightContinue: true,
+  optionsSuccessStatus: 200,
+  credentials: true,
+  maxAge: 3600,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors(corsOption));
 app.use(morgan(configVar.server.morgan));
 app.use(helmet());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec, { explorer: true }));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(spec, { explorer: true }),
+);
 app.use('/api', rootRouter);
-app.use((error: any, _request: Request, response: Response, _next: NextFunction) => {
-	console.error(error);
+app.use(
+  (error: any, _request: Request, response: Response, _next: NextFunction) => {
+    console.error(error);
 
-	return response.status(500).json({
-		message: '서버에러가 발생했습니다. 잠시후 다시 시도해주세요.',
-		error,
-	});
-});
+    return response.status(500).json({
+      message: '서버에러가 발생했습니다. 잠시후 다시 시도해주세요.',
+      error,
+    });
+  },
+);
 
 const initializeApp = async () => {
-	try {
-		if (env !== 'test') {
-			await AppDataSource.initialize();
-			await RedisClient.connect();
-		}
-		app.listen(port, () => {
-			console.log('connect database...');
-			console.log('connect redis server...');
-			console.log(`connect server at port number ${port}...`);
-		});
-	} catch (error) {
-		console.error(error);
-		RedisClient.disconnect();
-		throw new Error('Server Error');
-	}
+  try {
+    if (env !== 'test') {
+      await AppDataSource.initialize();
+      await RedisClient.connect();
+    }
+    app.listen(port, () => {
+      console.log('connect database...');
+      console.log('connect redis server...');
+      console.log(`connect server at port number ${port}...`);
+    });
+  } catch (error) {
+    console.error(error);
+    RedisClient.disconnect();
+    throw new Error('Server Error');
+  }
 };
 
 initializeApp();
