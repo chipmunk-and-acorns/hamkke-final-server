3948946d869fa180e399d7922b8d6ad37b1a3483
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2023-06-19 18:03:30 +0900

    댓글 기능 작성 (#34)
    * #33 - 댓글 기능 구현 및 문서화
    
    * #33 - 게시글 문서화 수정

---
+++ src/controller/comment.ts
@@ -0,0 +1,105 @@
+import { Request, Response } from 'express';
+import { findMemberById } from '../repository/member';
+import { findArticleById } from '../repository/article';
+import {
+  findCommentById,
+  findComments,
+  removeComment,
+  saveComment,
+} from '../repository/comment';
+import Comment from '../entity/comment';
+import { isEmpty } from 'lodash';
+
+export const createComment = async (request: Request, response: Response) => {
+  const { memberId } = response.locals;
+  const { content, articleId } = request.body;
+
+  try {
+    const member = await findMemberById(memberId);
+    const article = await findArticleById(articleId);
+
+    if (member && article) {
+      const newComment = new Comment();
+      newComment.article = article;
+      newComment.member = member;
+      newComment.content = content;
+      const savedComment = await saveComment(newComment);
+
+      return response.status(201).json({ comment: savedComment });
+    }
+
+    return response
+      .status(400)
+      .json({ message: '존재하지 않은 게시글입니다.' });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      message: '에러가 발생했습니다. 잠시후 다시 시도해주세요.',
+      error,
+    });
+  }
+};
+
+export const getComments = async (_request: Request, response: Response) => {
+  try {
+    const comments = await findComments();
+
+    return response.status(200).json({ comments });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      message: '에러가 발생했습니다. 잠시후 다시 시도해주세요.',
+      error,
+    });
+  }
+};
+
+export const updateComment = async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { content } = request.body;
+
+  try {
+    const comment = await findCommentById(parseInt(id), ['members']);
+
+    if (isEmpty(comment)) {
+      return response
+        .status(400)
+        .json({ message: '찾을 수 없는 게시글입니다.' });
+    }
+
+    comment.content = content;
+    const updateComment = await saveComment(comment);
+
+    return response.status(200).json({ comment: updateComment });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      message: '에러가 발생했습니다. 잠시후 다시 시도해주세요.',
+      error,
+    });
+  }
+};
+
+export const deleteComment = async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  try {
+    const comment = await findCommentById(parseInt(id));
+
+    if (isEmpty(comment)) {
+      return response
+        .status(400)
+        .json({ message: '존재하지 않는 게시글입니다.' });
+    }
+
+    await removeComment(comment);
+
+    return response.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      message: '에러가 발생했습니다. 잠시후 다시 시도해주세요.',
+      error,
+    });
+  }
+};
